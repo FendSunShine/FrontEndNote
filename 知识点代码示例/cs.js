ce91f0b8062f8fbb49350f63d5cce6b0d0ab170a
lixin <2635607847@qq.com>
2023-06-01 09:13:31 +0800

    6/1

--- 知识点代码示例/cs.js
+++ 知识点代码示例/cs.js
@@ -1,45 +1,11 @@
-const arr = [3,3,3,4,5,5,6,[1,2,3], [1,2,3], [3,2,1],
-{a:1, b:2, c:3, d:4},
-{a:1, b:2, c:3, d:5},
-{a:1, b:2, d:4, c:3}
-
-];
-function uniqueArr(arr){
-    const _isObj = (a) => typeof a === 'object' && a !== null
-    const _isSame = (o1, o2) => {
-        if (_isObj(o1) && _isObj(o2)){
-            // 都是对象
-            // 先判断key
-            const o1key = Object.keys(o1)
-            const o2key = Object.keys(o2)
-            if (o1key.length != o2key.length){
-                return false
-            } else {
-                // 循环所有key
-                for(let k of o1key){
-                    // 如果o1key有o2没有的
-                    if (!o2key.includes(k)) return false
-                    // 如果有就比较值
-                    // ！！！这里的值可能是多层次所以需要递归比较
-                    if (!_isSame(o1[k], o2[k])) return false
-                }
-            }
-            return true
-        } else {
-            return o1 === o2
-        }
-        
-    }
-    const uniArr = [...arr]
-    for (let i = 0; i < uniArr.length; i++) {
-        for (let j = i+1; j < arr.length; j++){
-            if (_isSame(uniArr[i], uniArr[j])){
-                uniArr.splice(j, 1)
-                j--
-            }
-        }
-    }
-    return uniArr
-}
-const uniArr = uniqueArr(arr)
-console.log(uniArr);0
+const list1 = [
+  {a:1, b:2},
+  {c:3, d:4},
+  {e:5, f:6},
+]
+const list2 = [...list1]
+// 由结果可看，扩展运算符只是第一次的深拷贝，第二层之后还是用的地址
+// 毕竟原数据也都是用的地址
+console.log(list1[1] === list2[1]);
+console.log(list1 === list2);
+console.log({a:1} === {a:1});
